3e100eab7949537d6fbd232eda35567be5f2b5a0
allenkim0129 <allenkim0129@gmail.com>
2022-02-11 01:06:20 -0800

    Added 3 types of monsters and spawn them Also they take demages

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    public int hp = 10;
-    public float speed = 2f;
+    public float m_hp;
+    public float m_speed;
+    public int m_level;
 
-    public Transform target;
+    public Transform m_target_transform;
 
-    private void Start()
+    public void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        m_target_transform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    public void Update()
+    {
+        Vector3 dir = m_target_transform.position - transform.position;
+        transform.Translate(dir * Time.deltaTime * m_speed, Space.World);
+        transform.LookAt(m_target_transform);
     }
 
-    private void Update()
+    // public void OnCollisionEnter(Collision collisionInfo)
+    // {
+    //     Debug.Log(collisionInfo.collider.name);
+    // }
+
+    public void takeDemage(float demage)
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * Time.deltaTime * speed , Space.World);
-        transform.LookAt(target);
+        m_hp -= demage;
+        // Debug.Log("demage: " + demage.ToString() + " hp: " + hp.ToString());
+        
+        if (m_hp < 0)
+        {
+            Debug.Log("Death");
+            dropExpGem();
+            Destroy(gameObject);
+        }
+    }
+
+    //TODO: Under Construction
+    private void dropExpGem()
+    {
+        Debug.Log("dropExpGem()");
+        //randomly drop a gem.
+        // As level goes higher, the posibility of droping better gem increase
+
+        return;
     }
 }
---
+++ Assets/SpiderKing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderKing : Monster
+{
+    SpiderKing()
+    {
+        m_hp = 500;
+        m_speed = .3f;
+        m_level = 3;
+    }
+}
